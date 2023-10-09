27e6621f2128accd7c25c038f43924416c5b3966
David Lai <laidav@gmail.com>
2023-10-09 18:37:11 -0400

    initial

---
+++ src/ticket.service.ts
@@ -0,0 +1,15 @@
+import { of } from 'rxjs';
+import { delay } from 'rxjs/operators';
+import { EventTypes, FetchPricePayload } from './Models/EventTypes';
+
+export class TicketService {
+  static prices = {
+    [EventTypes.ChiliCookOff]: 20,
+    [EventTypes.ItchyAndScratchyMovie]: 40,
+    [EventTypes.GrammarRodeo]: 50,
+  };
+
+  static getPrice({ event, qty }: FetchPricePayload) {
+    return of(TicketService.prices[event] * qty).pipe(delay(1000));
+  }
+}

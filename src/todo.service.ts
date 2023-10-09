6af6fcee4e0e7d94e9600d1fc2745c43cabfe5de
David Lai <laidav@gmail.com>
2023-10-09 17:02:58 -0400

    remove constructor

--- src/todo.service.ts
+++ src/todo.service.ts
@@ -3,8 +3,6 @@ import { delay } from 'rxjs/operators';
 import { UpdateTodoPayload } from './Models/Todos';
 
 export class TodoService {
-  constructor() {}
-
   static updateTodo(payload: UpdateTodoPayload): Observable<UpdateTodoPayload> {
     return of(payload).pipe(delay(2000));
   }

493c5a7fa47024db84963455d2277b340db185cc
David Lai <laidav@gmail.com>
2023-10-09 13:41:12 -0400

    add counter example

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,46 @@
-import { Reducer } from '@hub-fx/core';
+import { Reducer, Action } from '@hub-fx/core';
 import { useHub } from './Hooks/useHub';
 import { useObservable } from './Hooks/useObservable';
 import './App.css';
 
-const reducer: Reducer<number> = (state = 0) => state;
+// Actions
+const INCREMENT = 'INCREMENT';
+const increment = (): Action => ({ type: INCREMENT });
+
+const RESET = 'RESET';
+const reset = (): Action => ({ type: RESET });
+
+// Reducer function to handle state updates
+const reducer: Reducer<{ count: number }> = (state = { count: 0 }, action) => {
+  switch (action?.type) {
+    case INCREMENT:
+      return { count: state.count + 1 };
+    case RESET:
+      return { count: 0 };
+    default:
+      return state;
+  }
+};
 
 function App({ hub = useHub() }) {
   const state = useObservable(hub.store({ reducer }));
   return (
-    <div className="App">
-      <h1>{state}</h1>
-    </div>
+    <>
+      <h1>Counter</h1>
+      <div>
+        Count: {state?.count}
+        <br />
+        <br />
+        <button type="button" onClick={() => hub.dispatch(increment())}>
+          Increment
+        </button>
+        <br />
+        <br />
+        <button type="button" onClick={() => hub.dispatch(reset())}>
+          Reset Counter
+        </button>
+      </div>
+    </>
   );
 }
 

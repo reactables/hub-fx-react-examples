287ce367262cc574f6748e559824814b6e8722f5
David Lai <laidav@gmail.com>
2023-11-12 14:46:58 -0500

    add comments

--- src/Hooks/useReactable.ts
+++ src/Hooks/useReactable.ts
@@ -1,6 +1,10 @@
 import { useEffect, useState, useRef } from 'react';
 import { Reactable } from '@hub-fx/core';
 
+// React Strict Mode has bugs with clean up with refs so it breaks the useReactable hook as of now
+// See Bug: https://github.com/facebook/react/issues/26315
+// See Bug: https://github.com/facebook/react/issues/24670
+
 export const useReactable = <T, S>(reactable: Reactable<T, S>) => {
   const { state$, actions } = useRef(reactable).current;
   const [state, setState] = useState<T>();

5b76077a97833fe119a81b13bee3ec3e48e85000
David Lai <laidav@gmail.com>
2023-11-29 21:20:24 -0500

    update package

--- src/Counter.tsx
+++ src/Counter.tsx
@@ -1,8 +1,8 @@
-import { RxCounter } from '@hub-fx/examples';
-import { useReactable } from '@hub-fx/react-helpers';
+import { RxCounter } from '@reactables/examples';
+import { useReactable } from '@reactables/react-helpers';
 
 // See for implementation and tests:
-// https://github.com/hub-fx/hub-fx/tree/main/packages/examples/src/Counter
+// https://github.com/reactables/reactables/tree/main/packages/examples/src/Counter
 
 function Counter() {
   const {
